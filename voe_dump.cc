3d2f886291393a923b4ecc322a2cde0d72b01387
Ralph Giles <giles@mozilla.com>
2011-07-21 10:36:19 -0700

    Dump the available audio devices as well.
    The API returns a GUID as well as a name, but on my system
    (Linux x86_64 / Fedora 15) they contain the same data, so
    I didn't print them.

--- voe_dump.cc
+++ voe_dump.cc
@@ -11,9 +11,11 @@
 /* Test and dump an enumeration of voice engine capabilities */
 
 #include <stdio.h>
+#include <string.h>
 
 #include <voe_base.h>
 #include <voe_codec.h>
+#include <voe_hardware.h>
 
 using namespace webrtc;
 
@@ -21,9 +23,38 @@ int main(int argc, char *argv[])
 {
   VoiceEngine* voe = VoiceEngine::Create();
   VoEBase* base = VoEBase::GetInterface(voe);
+  VoEHardware* device = VoEHardware::GetInterface(voe);
   VoECodec* codec = VoECodec::GetInterface(voe);
 
   base->Init();
+
+  // Enumerate and dump devices
+  int num_devices;
+  device->GetNumOfRecordingDevices(num_devices);
+  fprintf(stderr, "Voice Engine reports %d recording devices:\n", num_devices);
+  for (int i = 0; i < num_devices; i++) {
+    const unsigned kMaxLength = 128;
+    char name[kMaxLength];
+    char guid[kMaxLength];
+    memset(name, 0, kMaxLength);
+    memset(guid, 0, kMaxLength);
+    device->GetRecordingDeviceName(i, name, guid);
+    fprintf(stderr, "%d\t%s\n", i, name);
+  }
+
+  device->GetNumOfPlayoutDevices(num_devices);
+  fprintf(stderr, "Voice Engine reports %d playback devices:\n", num_devices);
+  for (int i = 0; i < num_devices; i++) {
+    const unsigned kMaxLength = 128;
+    char name[kMaxLength];
+    char guid[kMaxLength];
+    memset(name, 0, kMaxLength);
+    memset(guid, 0, kMaxLength);
+    device->GetPlayoutDeviceName(i, name, guid);
+    fprintf(stderr, "%d\t%s\n", i, name);
+  }
+
+  // Enumerate and dump codecs
   int num_codecs = codec->NumOfCodecs();
   fprintf(stderr, "Voice Engine reports %d audio codecs:\n", num_codecs);
 
@@ -35,9 +66,10 @@ int main(int argc, char *argv[])
         i, c.plname, c.plfreq, c.channels == 1 ? "mono" : "");
   }
 
+  codec->Release();
+  device->Release();
   base->Terminate();
   base->Release();
-  codec->Release();
 
   return 0;
 }
